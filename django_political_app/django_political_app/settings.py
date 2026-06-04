f737c3fec5c5a6cdb5e99baadf583d765e20f1d1
Ethan Puype <43581889+NICHIKU@users.noreply.github.com>
2026-06-04 10:34:35 +0200

    Update ALLOWED_HOSTS to use environment variable

--- django_political_app/django_political_app/settings.py
+++ django_political_app/django_political_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['164.132.43.197', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
